dae83d1ea4bc16ddd289e31eabf759ab7fc55328
M Ibad Guthwara <43487906+ibadguthwara@users.noreply.github.com>
2019-01-20 23:30:28 +0700

    Initial commit

---
+++ src/pbo3ulang/pkg10116049/latihan7/integer/PBO3ULANG10116049Latihan7Integer.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo3ulang.pkg10116049.latihan7.integer;
+
+/**
+ *
+ * @author ibadguthwara
+ * NAMA 	     : M. Ibad Guthwara
+ * KELAS	     : PBO 3 ULANG
+ * NIM		     : 10116049
+ * DESKRIPSI PROGRAM : Program ini berisi program untuk menampilkan tipe integer/bilangan bulat
+ */
+public class PBO3ULANG10116049Latihan7Integer {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        byte b;
+        short s;
+        int i;
+        long l;
+        b = 120;
+        s = 32767;
+        i = 1_000_000_000;
+        l = 90000000000L;
+        
+        System.out.println("byte : " + b);
+        System.out.println("short : " + s);
+        System.out.println("int : "+ i);
+        System.out.println("long : " + l);
+    }
+    
+}
